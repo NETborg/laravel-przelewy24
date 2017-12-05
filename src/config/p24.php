11bc4b2ad9c242d684a6900be6109d55e2a9ae25
NETborg <mail@rolandkolodziej.com>
2017-12-05 21:24:21 +0100

    Moved P24 MODE to .env

--- src/config/p24.php
+++ src/config/p24.php
@@ -4,6 +4,6 @@ return [
     'merchant_id' => env('P24_MERCHANT_ID', 0),
     'pos_id' => env('P24_POS_ID', 0),
     'crc' => env('P24_CRC', null),
-    'mode' => 'sandbox',                        // `sandbox` || `live`
-    'route_return' => null,                     // provide route name where Client shoud be redirected on transaction cancellation
+    'mode' => env('P24_MODE', 'live'),          // `sandbox` || `live`
+    'route_return' => null,                     // provide route name where Client should be redirected on transaction cancellation
 ];
