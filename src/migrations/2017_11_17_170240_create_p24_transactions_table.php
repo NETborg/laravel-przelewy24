85ed2037b0db88c6c8c91ddece9447544ef8e5ec
Roland Kołodziej <roland@oferado.pl>
2019-04-28 16:12:37 +0200

    Making p24_sign nullable.

--- src/migrations/2017_11_17_170240_create_p24_transactions_table.php
+++ src/migrations/2017_11_17_170240_create_p24_transactions_table.php
@@ -35,7 +35,7 @@ class CreateP24TransactionsTable extends Migration
             $table->integer('p24_channel')->nullable();
             $table->integer('p24_shipping')->unsigned()->default(0);
             $table->string('p24_transfer_label', 20)->nullable();
-            $table->string('p24_sign', 100);
+            $table->string('p24_sign', 100)->nullable();
             $table->string('p24_encoding', 15)->default('UTF-8');
             $table->integer('p24_order_id')->nullable();
             $table->string('p24_statement')->nullable();
